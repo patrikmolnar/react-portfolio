700b5afab303ffa01e896139e4b2f9c20539cc16
Patrik Molnar <info@patrikmolnar.com>
2018-05-16 17:16:48 +0700

    prep for AWS

---
+++ upload-lambda.py
@@ -0,0 +1,49 @@
+import boto3 
+from botocore.client import Config
+import io
+import zipfile
+import mimetypes
+
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:ap-southeast-1:202433929926:DeployPortfolio')
+    
+    location = {
+        "bucketName": 'react.patrikmolnar.com',
+        "objectKey": 'reactbuild.zip'
+    }
+    
+    try:
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                    
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket = s3.Bucket('react.patrikmolnar.com')
+        build_bucket = s3.Bucket(location["bucketName"])
+        
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+        
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, 
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject="Project", Message="Project deployed successfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="There was an error while trying to deploy the project.")
+        raise
+    
+    return 'Deploy Pass!'
